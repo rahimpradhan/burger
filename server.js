ddc8f8cab134c2c2d3feca469189cd1460539a17
Rahim Pradhan <rahimpradhan@gmail.com>
2017-11-17 11:06:33 -0500

    WIP

--- server.js
+++ server.js
@@ -17,25 +17,9 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var mysql = require("mysql");
-
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "1234",
-  database: "burgers_db"
-});
-
 var routes = require("./controllers/burgers_controller.js");
 
 app.use("/", routes);
 
 app.listen(port);
 
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
